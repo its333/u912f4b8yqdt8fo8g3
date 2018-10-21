6c377eaffac4f3d2483a928bef250276e6b252f6
its333 <44331289+its333@users.noreply.github.com>
2018-10-21 15:20:29 -0400

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const Discord = require("discord.js")
 const { get } = require('snekfetch');
@@ -18,23 +17,3 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name: "cat"
 }
-=======
-
-const Discord = require("discord.js")
-
-module.exports.run = async (bot, message, args) => {
-  const cat = await get('http://aws.random.cat/meow').catch(() => null);
-  if (!cat || !cat.body) return message.reply('There was an error, please try again.');
-  return message.channel.send({
-      embed:
-      {
-          footer: { text: `Requested by ${message.author.tag}`, icon_url: message.author.displayAvatarURL },
-          image: { url: cat.body.file }
-      }
-  });
-}
-
-module.exports.help = {
-  name: "cat"
-}
->>>>>>> 313b4773e05803eb74b2411a8a093b3160a2a001
