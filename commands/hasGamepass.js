dcceee60ef9063fa238866093672c9e05426045c
its333 <44331289+its333@users.noreply.github.com>
2018-10-22 18:47:47 -0400

    Add files via upload

---
+++ commands/hasGamepass.js
@@ -0,0 +1,24 @@
+
+const Discord = require("discord.js")
+const { get } = require('snekfetch');
+const randomColor = require('randomcolor'); // import the script
+
+module.exports.run = async (bot, message, args) => {
+
+  if(args.length < 2){
+    message.channel.send(new Discord.RichEmbed()
+    .setColor(randomColor())
+    .addField("Usage", "hasgamepass {userid} {gamepass id}", true));
+    return;
+  };
+
+  const res = await get(`https://inventory.roblox.com/v1/users/${args[0]}/items/GamePass/${args[1]}`).catch(() => null);
+  if (!res || !res.body) return message.reply('There was an error, please try again.');
+  return message.channel.send(new Discord.RichEmbed()
+    .setColor(randomColor())
+    .addField("Ownership", Object.keys(res.body.data).length == 1, true));
+}
+
+module.exports.help = {
+  name: "hasgamepass"
+}
