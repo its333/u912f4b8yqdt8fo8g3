e55d97119502982b5677ba8c25342a2babf931c5
its333 <44331289+its333@users.noreply.github.com>
2018-11-14 21:36:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ bot.on("message", async message => {
     var generalChannel = message.guild.channels.find("name","general")
     //if(message.channel != patreonChannel && message.channel != generalChannel) return;
 
-    console.log(commandfile.help.hasOwnProperty("anyone"));
+    console.log(message.member.roles.find('name','Raddleton Patreon' || //require premission
+      commandfile.help.hasOwnProperty("anyone")));
     if(message.member.roles.find('name','Raddleton Patreon' || //require premission
       commandfile.help.hasOwnProperty("anyone"))){ //anyone can use
         commandfile.run(bot, message, args); //run command
